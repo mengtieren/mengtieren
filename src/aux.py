be89a7ef6438ac5779060a5a237fcdb384b15202
michele-rosato <michele@momimo.io>
2022-01-14 18:47:03 +0200

    pilot

--- src/aux.py
+++ src/aux.py
@@ -1,53 +1,126 @@
 import time
 import zmq
+import os
 import json
+from multiprocessing import Process, Queue
 from pyftdi.gpio import (GpioAsyncController)
 
 
-def recv_message(_socket):
+NUM_SSR_OUTPUTS = 8
+DEFAULT_DURATION = 1
 
+
+class Output:
+    def __init__(self, timer_mode = 1, invert_output = 0, duration = DEFAULT_DURATION):
+        self.duration = duration 
+        self.set_time = 0
+        self.timer_mode = timer_mode
+        self.invert_output = invert_output
+        self.state = 0
+
+    def set(self):
+        if self.timer_mode:
+            self.set_time = time.time()
+        self.state = 1
+
+    def unset(self):
+        self.state = 0
+
+    def change_output_mode(self, timer_mode, duration = DEFAULT_DURATION, invert_output = 0):
+        self.timer_mode = timer_mode
+        self.duration = duration
+        self.invert_output = invert_output
+
+    def check(self):
+        if self.timer_mode:
+            if self.state:
+                life = time.time() - self.set_time 
+                #print ("life, duration: ", life, self.duration)
+                if life >= self.duration:
+                    print ("resetting")
+                    self.state = False
+        return self.state ^ self.invert_output 
+
+
+def recv_messages(msg_q):
     #  Wait for next request from client
+    address_pub_logic = os.environ.get('SERVER_PUB_LOGIC_CONNECT_URI')
 
-    print("Waiting for msg from logic...")
-    message = _socket.recv()
+    while True:
+        context = zmq.Context()
+        socket = context.socket(zmq.SUB)
+        socket.connect(address_pub_logic)
+        socket.subscribe("")
+        print("Waiting for msg from: ", address_pub_logic)
+        message = socket.recv()
+        #print("Recieved msg from logic ...")
+        msg_q.put(json.loads(message))
 
-    print("Recieved msg from logic ...")
-    message_json = json.loads(message)
+# def send_status(_socket, inputs):
+#     status_msg = {
+#         "message": "aux_status",
+#         "inputs": inputs
+#     }
+#     print ("sending status message: \n", status_msg)
+#     _socket.send_string(json.dumps(status_msg)) 
 
-    ok_msg = {
-        "message": "aux_status",
-        "status_code": "ok"
-    }
+def main(ssr_bank, opto_bank):
 
-    #  Send reply back to client
-    _socket.send_string(json.dumps(ok_msg)) 
-    return message_json
+    print ("in Aux")
+    msg_q = Queue()
+    msg_proc = Process(target=recv_messages, args=(msg_q, ), daemon = True)
+    msg_proc.start()
 
-def main():
-    ssr_bank = GpioAsyncController()
-    ssr_bank.configure("ftdi:///2", direction=0xFF, frequency=1e3, initial=0x0)
-    opto_bank = GpioAsyncController()
-    opto_bank.configure("ftdi:///3", direction=0x00, frequency=1e3, initial=0x0)
-    gpio_bank = GpioAsyncController()
-    gpio_bank.configure("ftdi:///4", direction=0x00, frequency=1e3, initial=0x0)
+    aux_board_present = False
 
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:5004")
+    while aux_board_present == False:
+        try:
+            ssr_bank.configure("ftdi:///2", direction=0xFF, frequency=1e3, initial=0x0)
+            opto_bank.configure("ftdi:///3", direction=0x00, frequency=1e3, initial=0x0)
+            aux_board_present = True
+        except:
+            print (time.time(), "No Aux board connected")
+            time.sleep(1)
 
+
+
+    outputs = []
+    for i in range (NUM_SSR_OUTPUTS):
+        outputs.append(Output())
+    message_type = os.environ.get("AUX_PULSE_MESSAGE")
     while(1):
-        message_json = recv_message(socket)
+        if msg_q.qsize() > 0:
+            
+            message = msg_q.get()
+            if message["message"] == message_type:
+                print (message)
+                char0 = message["feed_name"][0]
+                if char0 in ["1","2","3","4","5","6","7","8"]:
+                    print("setting output", char0)
+                    outputs[int(char0)-1].set()
 
-        if message_json["alarm"] == "activated":
-            print ("ALARM ACTIVATED")
-            ssr_bank.write(0x01) 
-            time.sleep(2)
-            ssr_bank.write(0x00) 
+          
+            
+
+                
+
+        
+        outputs_total = 0
+        for i in range(len(outputs)):
+            outputs_total += (2**i)*outputs[i].check()
+            ssr_bank.write(outputs_total)
+        
+        #inputs = opto_bank.read()
+        #send_status(socket, inputs)
 
 if __name__ == '__main__':
+    print ("Aux running")
+    ssr_bank = GpioAsyncController()
+    opto_bank = GpioAsyncController()
     try: 
-        print ("Aux running")
-        main()
+        main(ssr_bank, opto_bank)
     except KeyboardInterrupt:
-        print ("\nexiting")
+        ssr_bank.close()
+        opto_bank.close()
+        print ("\nExiting Aux")
 
--- src/xmit.py
+++ src/xmit.py
@@ -1,2 +1,20 @@
+import zmq
+import time
+import json
+
+ctx = zmq.Context()
+sock = ctx.socket(zmq.PUB)
+sock.bind("tcp://*:4321")
+
+print("Starting loop...")
+i = 1
 while True:
-    pass
+    msg = json.dumps({"message":"ana_aux_pulse","cort_id":"F_1_Outside","name": str(i) + "Testing","index":i})
+    sock.send_string(msg)
+    print("Sent string: %s ..." % msg)
+    i += 1
+    if i > 8: i = 1
+    time.sleep(0.2)
+
+sock.close()
+ctx.term()
